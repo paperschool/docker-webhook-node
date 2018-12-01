661ed01077399564a31c53b413e4de8ed8354fcf
paperschool <dominic.jomaa@gmail.com>
2018-12-01 14:34:04 +0000

    added cli interface and helper methods to make program easier to use

--- config.js
+++ config.js
@@ -1,41 +1,56 @@
 
+const chalk = require('chalk');
+
 const config = require('./config.json');
 
 const shell = require('shelljs');
 
 const fs = require('fs');
 
+const log = console.log;
+
 const methods = {};
 
 methods.handle = ({ name, token },body = null) => {
 
-    console.log("Checking Params!")
+    log(chalk.white('Checking Params!'))
 
     // checking if params are supplied
     if(!name || !token) return false;
 
-    console.log("Checking Name Config!")
-
     // checking if name param has matching object
-    if(!config.hasOwnProperty(name)){
-        console.log('Project Object missing properties')
+    if(!config.project.hasOwnProperty(name)){
+        log(chalk.red(`Project with Name : ${name} does not exist!`));
         return false;
+    } else {
+        log(chalk.green('Checking Name Config!'));
     }
 
-    if(!methods.validateProject(name)) return false;
-
-    console.log("Checking Has Body!")
+    if(!methods.validateProject(name)){
+        log(chalk.red('Project config not validated correctly!'));
+        return false;
+    } else {
+        log(chalk.green('Project Config Valid!'));
+        
+    }
 
     if(!body) return false;
 
-    console.log("Authenticating!")
-
     // checking name and token combination is successful
-    if(!methods.authenticate(name,token)) return false;
+    if(!methods.authenticate(name,token)){
+        log(chalk.red('Request Token not valid!'));
+        return false;
+    } else {
+        log(chalk.green('Request Token Valid!'));
+    }
 
-    console.log("Checking Timeout!")
 
-    if(!methods.timeout(name)) return true;
+    if(!methods.timeout(name)){
+        log(chalk.yellow('Image still in date, nothing to do!'));
+        return true;
+    } else {
+        log(chalk.green('Image out of date!'));
+    }
 
     return methods.execute(name,body);
 
@@ -63,59 +78,150 @@ methods.authenticate = (name,token) => {
 
 // checking if last update was less than 5 minutes ago ( to block repeated requests )
 methods.timeout = name => {
-    console.log(`Checking timeout Property In ${name}`)
+    log(chalk.yellow(`Checking timeout Property In ${name}`));
     return Date.now() - config.project[name].last >= 320000
 }
 
 methods.updateTimeout = name => {
-    console.log(`Updating timeout Property In ${name}`)
+    log(chalk.yellow(`Updating timeout Property In ${name}`));
     config.project[name].last = Date.now();
-    fs.writeFileSync('./config.json', JSON.stringify(config));  
+    methods.configWrite(config);
 }
 
-methods.login = () => {
+methods.setLogin = (username, password) => {
+    log(chalk.yellow(`Updating Credential Properties`));
+    config.cred = {
+        "user" : username,
+        "pass" : password    
+    }
+    methods.configWrite(config);
+}
 
-    if(!(config.cred.pass || config.cred.user)) {
-        console.log("Docker Login Failed - No Login Credentials Found!")
+methods.resetLogin = () => {
+    config.cred = {
+        "user" : null,
+        "pass" : null    
+    }
+    methods.configWrite(config);
+    log(chalk.yellow("Docker login credentials reset!"))
+}
+
+methods.login = (test = false, username, password) => {
+
+    if(test && !(username && password) ) {
+        log(chalk.red("Docker Login Failed - No Login Credentials Supplied!"));
         return false;
     }
 
+    username = username || config.cred.user;
+    password = password || config.cred.pass;
+
+    if(!test && !(username && password)) {
+        log(chalk.red("Docker Login Failed - No Login Credentials found in Config!"));
+        return false;
+    }
+    
     // login to docker
-    if(shell.exec(`docker login -u ${config.cred.user} -p ${config.cred.pass}`).code !== 0){
-        console.log("Docker Login Failed...")
+    if(shell.exec(`docker login -u ${username} -p ${password}`).code !== 0){
+        log(chalk.red("Docker Login Failed..."));
         return false;
     } else {
-        console.log("Docker Login Succeded!");        
+        log(chalk.green("Docker Login Succeded!"));        
         return true
     }
 
+}
+
+methods.removeProject = name => {
+    
+    if(!methods.find(name)){
+        log(chalk.red("Docker Project not found!"));
+        return false;
+    }
+
+    delete config.project[name];
+
+    methods.configWrite(config)
+
+    log(chalk.green(`Docker project ${name} removed successfully!`));
+
+}
+
+methods.modifyProject = ( name, portIn, portOut, token ) => {
+
+    if(methods.find(name)){
+        
+        log(chalk.yellow(`Docker Project ${name} exists already, overwriting existing project config!`));
+    
+        config.project[name] = {
+            "portIn"  : portIn,
+            "portOut" : portOut,
+            "token"   : token,
+            "last"    : 0
+        } 
+    
+        methods.configWrite(config);
+        
+    } else {
+
+        log(chalk.green(`Docker project ${name} added successfully`));
+
+    }
+
+}
+
+
+
+methods.resetConfig = () => {
+
+    const newConfig = {
+        "cred" : {
+            "user" : null,
+            "pass" : null
+        },
+        "project" : {}
+    }
+
+    methods.configWrite(newConfig);
+    
+    log(chalk.green('Docker config reset!'))
+}
+
+methods.configWrite = c => {
+    fs.writeFileSync('./config.json', JSON.stringify(c, null, 4));
+}
+
+methods.printConfig = () => {
+    log();
+    log(JSON.stringify(config,null,4))
+    log();
 }
 
 methods.execute = (name, body) => {
 
     // login to docker
     if(shell.exec(`docker login -u ${config.cred.user} -p ${config.cred.pass}`).code !== 0){
-        console.log("Docker Login Failed...")
+        log(chalk.red("Docker Login Failed..."));
         return false;
     }
 
     if(shell.exec(`docker pull ${body.repository.repo_name}`).code !== 0){
-        console.log("Docker Image pulled unsuccessfully!")
+        log(chalk.red("Docker Image pulled unsuccessfully!"));
         return false;
     }
 
     if(shell.exec(`docker stop ${body.repository.name}`).code !== 0){
-        console.log("Docker Image does not exist or incorrect name, nothing stopped!")
+        log(chalk.red("Docker Image does not exist or incorrect name, nothing stopped!"));
     }
 
     if(shell.exec(`docker rm ${body.repository.name}`).code !== 0){
-        console.log("Docker Image does not exist or incorrect name, nothing removed!")
+        log(chalk.red("Docker Image does not exist or incorrect name, nothing removed!"));
     }
 
     if(shell.exec(`docker run -d --name ${body.repository.name} -p ${config.project[name].portOut}:${config.project[name].portIn} ${body.repository.repo_name}`).code !== 0){
-        console.log("Docker !")
+        log(chalk.red("Docker !"));
     } else {
-        console.log(`Docker Image ${body.repository.repo_name} Started Successfully!`)
+        log(chalk.green(`Docker Image ${body.repository.repo_name} Started Successfully!`));
     }
 
     methods.updateTimeout(name);
---
+++ server.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+'use strict';
+
+const chalk = require('chalk')
+
+const bodyparser = require('body-parser');
+
+const express = require('express');
+
+const app = express();
+
+const config = require('./config.js');
+
+const PORT = 7777;
+
+const log = console.log;
+
+module.exports = () => {
+
+    if(!config.login()) return;
+    
+    app.use(bodyparser.json());
+    
+    app.listen(PORT,() => {
+        log(chalk.green(`Docker Webhook - Server Listening on PORT ${PORT}`))
+    });
+    
+    app.get('/',(req,res) => {
+
+        log(chalk.green('Webhook Front End Hit!'));
+        res.send('Docker Webhook Frontend is Up!');
+    });
+    
+    app.post('/',(req,res) => {
+        
+        log(chalk.green('Webhook Hit!'));
+                
+        if(config.handle(req.query,req.body)){
+            log(chalk.green('Hook Triggered Restart!'));
+            res.send('Hook Triggered Restart!');
+        } else {
+            log(chalk.red('Incorrect Config'));
+            res.send('Incorrect Config!');
+        }
+    
+    })
+
+}
+
